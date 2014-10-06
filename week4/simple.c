3a925543cae603676483c4170d2bc102a1b2e913
x00068315 <x00068315@ittd.ie>
2014-10-06 20:45:01 +0100

    changed the file

--- week4/simple.c
+++ week4/simple.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include void test(int value);
+void test(int value);
 
 int main (int argc, char** argv){
 test(2);
 
 }
 
-{
+
 //this function returns niothing and takes no parameters
 //and takes one parameter and integer
-void test(int value)
- print("Greeting from test()\n");
+void test(int value){
+ printf("Greeting from test()\n");
 }
